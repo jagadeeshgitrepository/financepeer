c4a00c4f115fa687784a3c4aa365913ed133e441
jagadeesh <jagadeeshnoddy12@gmail.com>
2022-03-20 14:10:33 +0000

    sixteen

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ app.post("/login/", async (request, response) => {
 });
 
 app.post("/book/", authenticateToken, async (request, response) => {
-  response.send({ hai: "hai" });
+  response.send(request.body);
 });
 
 app.get("/getBooks/", authenticateToken, async (request, response) => {
